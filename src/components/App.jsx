ab7dc4bf64158cbb7e2e58ddbb0a42efffafaf16
Mitya-Kupriianov <MityaBlow@gmail.com>
2022-12-23 12:25:29 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,10 @@
 import AppRoutes from 'Routes/Routes';
-import NoticeCategoryItem from 'components/Notices/NoticeCategoryItem';
 
 export default function App() {
   return (
     <>
       <AppRoutes />
       <h1>Start Project Petly</h1>
-      <NoticeCategoryItem />
     </>
   );
 }
