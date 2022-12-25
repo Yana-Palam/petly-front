5d871216598a203ae730d04b12fd9ae29dddd2dc
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-12-25 04:13:39 +0200

    added search input

--- src/components/Notices/NoticesSearch/NoticesSearch.jsx
+++ src/components/Notices/NoticesSearch/NoticesSearch.jsx
@@ -1,5 +1,44 @@
+import { useState } from 'react';
+import { useSearchParams } from 'react-router-dom';
+
+import { BsSearch } from 'react-icons/bs';
+
 function NoticesSearch() {
-  return <div>NoticesSearch</div>;
+  const [search, setSearch] = useState('');
+  const [searchParams, setSearchParams] = useSearchParams();
+
+  const query = searchParams.get('query');
+
+  const handleChange = ({ target }) => {
+    const { value } = target;
+    setSearch(value);
+  };
+
+  const handleFormSubmit = e => {
+    e.preventDefault();
+    const query = e.target.query.value;
+    setSearchParams({ query });
+    setSearch('');
+  };
+
+  return (
+    <>
+      <form onSubmit={handleFormSubmit}>
+        <input
+          onChange={handleChange}
+          type="text"
+          name="query"
+          autoFocus
+          autoComplete="off"
+          placeholder="Search"
+          value={search}
+        />
+        <button type="submit" aria-label="search button">
+          <BsSearch size="32px" />
+        </button>
+      </form>
+    </>
+  );
 }
 
 export default NoticesSearch;
--- src/page/noticesPage/NoticesPage.jsx
+++ src/page/noticesPage/NoticesPage.jsx
@@ -1,9 +1,11 @@
 import Container from 'components/Common/Container';
 import AddNoticeButton from 'components/Notices/AddNoticeButton';
+import NoticesSearch from 'components/Notices/NoticesSearch';
 import NoticesCategoriesNav from 'components/Notices/NoticesCategoriesNav';
 function NoticesPage() {
   return (
     <>
+      <NoticesSearch />
       <NoticesCategoriesNav />
       <Container>
         <AddNoticeButton />
