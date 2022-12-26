b12707a1ada7d57d1f00ad886141c0933a838788
Alena-Abharian <alenar2531@gmail.com>
2022-12-27 01:19:14 +0200

    privateRouter change

--- src/redux/notice/noticeOperations.js
+++ src/redux/notice/noticeOperations.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 import { createAsyncThunk } from '@reduxjs/toolkit';
-axios.defaults.baseURL = 'https://petly-bc26.cyclic.app';
+// axios.defaults.baseURL = 'https://petly-bc26.cyclic.app';
 
 const token = {
   set(token) {
