296c35d47c4e16e2834d3a56f35076dbf4e78318
Aleksandr <102595282+AleksandrStarkov@users.noreply.github.com>
2022-12-27 12:54:48 +0200

    modalPets

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,16 @@
 import AppRoutes from 'Routes/Routes';
-
+// import ModalAddsPet from './User/ModalAddsPet';
+import PetsData from './User/PetsData';
+import PetsList from './User/PetsList';
 
 export default function App() {
   return (
     <>
-      <AppRoutes/>
+      <AppRoutes />
+      <h1>Start Project Petly</h1>
+      {/* <ModalAddsPet /> */}
+      <PetsData />
+      <PetsList />
     </>
   );
 }
