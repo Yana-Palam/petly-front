d3f05261b523f9485948ad9de315a1e31207179e
Maksym-Holovachuk <mgolovachuk@gmail.com>
2022-12-29 02:45:11 +0200

    fixed notice page and add getBtnInfo()

--- src/page/noticesPage/NoticesPage.jsx
+++ src/page/noticesPage/NoticesPage.jsx
@@ -1,33 +1,45 @@
 import { useState, useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { fetchByCategory } from 'redux/notice/noticeOperations';
-import { selectNoticeState } from 'redux/notice/noticeSelectors';
-import { useLocation, useSearchParams } from 'react-router-dom';
+import { useLocation, useSearchParams, useNavigate } from 'react-router-dom';
+
 import useToggleModal from 'hooks/toggleModal';
 
+import { selectAccessToken } from 'redux/auth/authSelectors';
+import { fetchByCategory } from 'redux/notice/noticeOperations';
+import { selectNoticeState } from 'redux/notice/noticeSelectors';
+
+// Components
 import Container from 'components/Common/Container';
 import Modal from 'components/Common/Modal/Modal';
-import AddNoticeButton from 'components/Notices/AddNoticeButton';
 import NoticesSearch from 'components/Notices/NoticesSearch';
 import NoticesCategoriesNav from 'components/Notices/NoticesCategoriesNav';
+import AddNoticeButton from 'components/Notices/AddNoticeButton';
 import NoticesCategoriesList from 'components/Notices/NoticesCategoriesList';
 import NoticeNotFound from 'components/Notices/noticeNotFound';
-import { Title } from './NoticesPage.styled';
 import Loader from 'components/Loader';
+import { Title } from './NoticesPage.styled';
+
+const initialState = {
+  search: '',
+  btnType: '',
+  btnId: '',
+};
 
 function NoticesPage() {
+  const [state, setState] = useState(initialState);
+
+  const dispatch = useDispatch();
+  const token = useSelector(selectAccessToken);
   const { notices, isLoading } = useSelector(selectNoticeState);
   const [searchParams, setSearchParams] = useSearchParams();
+
   const { isOpen, openModal, closeModal, handleBackdropClick, handleKeyDown } =
     useToggleModal();
-  const dispatch = useDispatch();
-  const [search, setSearch] = useState(
-    '' // searchParams.get('q') === null ? '' : searchParams.get('q')
-  );
-
-  const [idNotice, SetIdNotice] = useState();
 
   const path = useLocation().pathname;
+  let navigate = useNavigate();
+
+  console.log('notices', notices);
 
   useEffect(() => {
     const q = searchParams.get('q');
@@ -37,34 +49,39 @@ function NoticesPage() {
       // dispatch({ category, q: search });
       // setSearch('');
     } else {
-      console.log(222222, { path, q: search });
-      // if (category.split('/')[2] !== 'undefined') {
-      const req = path.split('/')[2];
-      dispatch(fetchByCategory(req));
+      console.log(222222, { path, q: state.search });
+      const category = path.split('/')[2];
+      dispatch(fetchByCategory(category));
       // }
     }
-  }, [search, path, searchParams, dispatch]);
+  }, [state.search, path, searchParams, dispatch]);
 
+  /**Select notice by id  */
   const getNoticeById = useMemo(() => {
-    const notice = notices?.find(item => item._id === idNotice);
+    const notice = notices?.find(item => item._id === state.btnId);
     return notice;
-  }, [notices, idNotice]);
+  }, [notices, state.btnId]);
 
-  // const toggleFavorite = (id, isFav) => {
-
-  // };
-
+  /**Search info by search form */
   const handleSearch = q => {
-    setSearch(q);
+    setState(prevState => ({
+      ...prevState,
+      search: q,
+    }));
     setSearchParams({ q });
   };
 
-  const getIdNotice = (btnId, btnType) => {
+  /**Get button-id and button-dataset*/
+  const getBtnInfo = (btnId, btnType) => {
     //TODO прописати логіку в залежності від кнопки
-    console.log('btnId', btnId);
-    console.log('btnType', btnType);
-    console.log(notices);
-    SetIdNotice(btnId);
+    setState(prevState => ({
+      ...prevState,
+      btnType,
+      btnId,
+    }));
+    if (token) {
+      navigate('/login');
+    }
     openModal();
   };
 
@@ -75,10 +92,17 @@ function NoticesPage() {
           handleBackdropClick={handleBackdropClick}
           handleKeyDown={handleKeyDown}
         >
-          <div>Modal Windows for {getNoticeById._id}</div>
-          <h2>{getNoticeById.title}</h2>
-          <img src={getNoticeById.avatarURL} alt={getNoticeById.title} />
-          <button onClick={closeModal}>close</button>
+          {state.btnType?.favorite && <p>Favorite</p>}
+          {state.btnType?.modal && (
+            <>
+              <div>Modal Windows for {getNoticeById._id}</div>
+              <h2>{getNoticeById.title}</h2>
+              <img src={getNoticeById.avatarURL} alt={getNoticeById?.title} />
+              <button onClick={closeModal}>close</button>
+            </>
+          )}
+          {state.btnType?.delete && <p>Delete</p>}
+          {state.btnType?.add && <p>Add pet</p>}
         </Modal>
       )}
 
@@ -89,11 +113,12 @@ function NoticesPage() {
         {isLoading ? (
           <Loader />
         ) : Boolean(notices?.length > 0) ? (
-          <NoticesCategoriesList notices={notices} getIdNotice={getIdNotice} />
+          <NoticesCategoriesList notices={notices} getBtnInfo={getBtnInfo} />
         ) : (
           <NoticeNotFound />
         )}
-        <AddNoticeButton />
+
+        <AddNoticeButton getBtnInfo={getBtnInfo} />
       </Container>
     </>
   );
