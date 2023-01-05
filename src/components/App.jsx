bf315509047797095bc3380b55a3709d97dee6af
unknown <tana.palam@gmail.com>
2023-01-05 13:34:57 +0200

    added axios interceptors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,24 +1,12 @@
 import AppRoutes from 'Routes/Routes';
 import { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
-import { getUserInfo, refresh } from '../redux/auth/authOperations';
-import {
-  selectAccessToken,
-  selectErrorAuth,
-} from '../redux/auth/authSelectors';
+import { getUserInfo } from '../redux/auth/authOperations';
+import { selectAccessToken } from '../redux/auth/authSelectors';
 
 export default function App() {
   const dispatch = useDispatch();
   const isAuth = useSelector(selectAccessToken);
-
-  const errorAuth = useSelector(selectErrorAuth);
-  const errorNotice = useSelector(state => state.notice.error);
-
-  useEffect(() => {
-    if (errorAuth || errorNotice) {
-      dispatch(refresh());
-    }
-  }, [dispatch, errorAuth, errorNotice]);
 
   useEffect(() => {
     if (isAuth) {
