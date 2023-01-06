33b4eb7788eb417eabfd32f400bcc5d229a04014
unknown <tana.palam@gmail.com>
2023-01-06 22:55:13 +0200

    changed regex password

--- src/components/Form/LoginForm/ValidationSchema.js
+++ src/components/Form/LoginForm/ValidationSchema.js
@@ -1,6 +1,6 @@
 import * as yup from 'yup';
 
-export const passwordRexExp = /^[a-zA-Z0-9]+$/;
+export const passwordRexExp = /^[a-zA-Zа-яА-Я0-9]+$/;
 
 export const emailRegExp =
   /^[a-zA-Z0-9]+[a-zA-Z0-9_-]+@[a-zA-Z0-9-]+.[a-zA-Z0-9]+$/;
