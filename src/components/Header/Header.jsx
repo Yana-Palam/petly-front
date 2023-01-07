422aeb28152677d2cf5be548bb436fcaf93aafd1
unknown <tana.palam@gmail.com>
2023-01-07 17:40:50 +0200

    fix

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { selectAccessToken } from 'redux/auth/authSelectors';
+import { selectIsLoggedIn } from 'redux/auth/authSelectors';
 import useScrollLock from 'hooks/useScrollLock';
 import NotLoggedIn from './NotLoggedIn';
 import LoggedIn from './LoggedIn';
@@ -8,7 +8,7 @@ import { HeaderStyled } from './Header.styled';
 
 function Header() {
   const [mobileMenuIsOpen, setMobileMenuIsOpen] = useState(false);
-  const isLogin = useSelector(selectAccessToken);
+  const isLogin = useSelector(selectIsLoggedIn);
   const { lockScroll, UnlockScroll } = useScrollLock();
 
   const toggleMenu = () => {
